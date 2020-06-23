58acecc7c7d412cb9cc4c8d3333a0c5035a1450f
Mingfang Jiao <mingfang.jiao@student.unsw.edu.au>
2020-06-23 22:12:16 +1000

    update

--- src/intern/fitbit.java
+++ src/intern/fitbit.java
@@ -7,35 +7,32 @@ import java.io.*;
 
 public class fitbit {
 	public static void main(String[] args) {
-		System.out.println("Enter Input:");
-		
 		FileInputStream file = null;
 		try {
-			file = new FileInputStream("./Trainee.java");
+			file = new FileInputStream("src/intern/input.txt"); // open the input file
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        Scanner sr = new Scanner(file);
+        Scanner sr = new Scanner(file); // scan input file line by line
         String grid = sr.nextLine();
         int grid_x = grid.charAt(0);
         int grid_y = grid.charAt(2);
-        ArrayList<Trainee> array = new ArrayList<Trainee>();
+        ArrayList<Trainee> array = new ArrayList<Trainee>(); // creating arraylist to store the output
         
         while( sr.hasNextLine() ){
             String position = sr.nextLine();
             if (position.length() < 1) {
             	break;
             }
-            int x = Character.getNumericValue(position.charAt(0));
+            int x = Character.getNumericValue(position.charAt(0));  
             int y = Character.getNumericValue(position.charAt(2));
             char ori = position.charAt(4);
-            Trainee ee = new Trainee(x,y,ori);
+            Trainee ee = new Trainee(x,y,ori);  // initialize the trainee
             String move = sr.nextLine();
-            for(int i = 0; i < move.length(); i++) {
+            for(int i = 0; i < move.length(); i++) {  // move the trainee in the grid
             	char c = move.charAt(i);
             	ee.move(c);
-            	if (ee.getx() > grid_x || ee.gety() > grid_y || ee.getx() < 0 || ee.gety() < 0 ) {
+            	if (ee.getx() > grid_x || ee.gety() > grid_y || ee.getx() < 0 || ee.gety() < 0 ) {  // check if the trainee out of grid
             		System.out.println("Exception thrown  : out of Grid");
             		return;
             	}
@@ -43,9 +40,9 @@ public class fitbit {
             array.add(ee);
         }
         System.out.println("Output is:");
-        for (Trainee t : array) {
-        	System.out.println(t.getx() + " " + t.gety() + " "+ t.getori());
+        for (Trainee t : array) {                                              
+        	System.out.println(t.getx() + " " + t.gety() + " "+ t.getori());  // print the output
         }
-        sr.close();
+        sr.close(); //close the file
     }
 }
